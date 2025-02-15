5b4545544670bb100224f09808172b971505283b
Yuda <iudasvastika@gmail.com>
2025-02-15 16:46:41 +0800

    feat: hero slider using swiper

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  function initSwiper(selector, revealClass) {
+    return new Swiper(selector, {
+      loop: true,
+      autoplay: { delay: 3000, disableOnInteraction: false },
+      speed: 1000,
+      allowTouchMove: false,
+      effect: "fade",
+      fadeEffect: { crossFade: false },
+      on: {
+        init: function () {
+          let firstSlide = $(`${selector} .swiper-slide-active img`);
+          setTimeout(() => {
+            firstSlide.addClass(`slider__zoom ${revealClass}`);
+            setTimeout(() => firstSlide.removeClass(revealClass), 1000);
+          }, 200);
+        },
+        slideChangeTransitionStart: function () {
+          let activeSlide = $(`${selector} .swiper-slide-active img`);
+          activeSlide.addClass(revealClass);
+          setTimeout(() => activeSlide.removeClass(revealClass), 1000);
+        },
+        slideChangeTransitionEnd: function () {
+          let activeSlide = $(`${selector} .swiper-slide-active img`);
+          activeSlide.addClass("slider__zoom");
+          setTimeout(() => activeSlide.removeClass("slider__zoom"), 4000);
+        },
+      },
+    });
+  }
+
+  initSwiper(".slider__left", "slider__slideReveal");
+  initSwiper(".slider__right", "slider__slideRevealReverse");
+});
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml">
 	<head>			
-		<title>title</title>
+		<title>OSOI</title>
 		
 		<meta charset="UTF-8">
 		<meta name="description" content="description">
@@ -34,12 +34,52 @@
 		
 		<link href="" rel="canonical">
 		
+		<link rel="stylesheet" href="./assets/vendors/animate-css/animate.min.css" />
+		<link rel="stylesheet" href="./assets/vendors/swiper/swiper-bundle.min.css" />
 		<link rel="stylesheet" type="text/css" href="./assets/css/style.css?<?= time() ?>">
 	</head>
-	<body>
+	<body style="position:relative;">
 		
 		<header class="header">
-			
+			<div class="header__logo">
+				<svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 602 210" width="602" height="210">
+					<title>logo</title>
+					<style>
+						.s0 { fill: #ffffff } 
+					</style>
+					<path id="I" class="s0" d="m556.1 23.7h44.7v160.6h-44.7z"/>
+					<path id="O" class="s0" d="m439.5 187.5q-18.6 0-34.5-6.5-15.8-6.6-27.3-17.8-11.5-11.3-18-26.4-6.6-15.1-6.6-32.3v-0.5q0-17.2 6.6-32.5 6.5-15.2 18.2-26.6 11.7-11.3 27.6-17.9 15.8-6.5 34.4-6.5 18.6 0 34.4 6.5 15.9 6.6 27.4 17.8 11.4 11.2 18 26.4 6.5 15.1 6.5 32.3v0.5q0 17.2-6.5 32.5-6.6 15.2-18.3 26.6-11.7 11.3-27.5 17.9-15.9 6.5-34.4 6.5m0.3-40.1q9.2 0 16.8-3.5 7.6-3.4 12.9-9.3 5.3-5.8 8.3-13.6 3-7.8 3-16.5v-0.5c0-5.8-1-11.3-3-16.5-2-5.2-4.9-9.8-8.5-13.8q-5.6-5.9-13.2-9.5-7.6-3.6-16.8-3.6-9.2 0-16.7 3.5-7.4 3.4-12.9 9.3-5.4 5.8-8.3 13.6-2.8 7.8-2.8 16.5v0.5c0 5.8 1 11.3 3 16.5 2 5.2 4.8 9.8 8.4 13.8q5.4 6 13 9.5c5 2.4 10.7 3.6 16.8 3.6"/>
+					<path id="S" class="s0" d="m266.3 187.1q-21.5 0-41-6.7-19.3-6.7-34.3-20.1l24.8-29.7q24.1 19.3 52.1 19.3 9 0 13.7-2.9c3.1-1.9 4.7-4.6 4.7-8.1v-0.5q0-2.5-1.1-4.4-1.2-2-4.3-3.8-3.1-1.8-8.2-3.5-5.2-1.8-13.5-3.6-12.8-3-23.8-6.5-11.1-3.6-19.2-9.2-8.2-5.6-12.9-14-4.7-8.3-4.7-20.7v-0.4c0-7.5 1.4-14.4 4.2-20.7q4.3-9.4 12.4-16.2 8.2-6.9 19.7-10.7 11.5-3.8 25.9-3.8 20.7 0 36.9 5.5 16.2 5.4 29.3 16l-22.3 31.5q-11-7.8-22.6-11.8-11.6-4-22.4-4-8.1 0-12.1 3-4 3-4 7.3v0.5q0 2.7 1.3 4.8 1.2 2 4.3 3.8 3.1 1.7 8.5 3.4 5.4 1.7 13.7 3.5 13.8 3 24.9 7c7.4 2.7 13.7 5.9 18.9 9.8 5.3 3.8 9.2 8.4 12 13.8q4.1 8.2 4.1 19.4v0.4q0 12.4-4.7 22-4.7 9.7-13.2 16.4-8.5 6.8-20.4 10.3-12 3.6-26.7 3.6"/>
+					<path id="O" class="s0" d="m87.5 187.5q-18.6 0-34.4-6.5-15.9-6.6-27.4-17.8-11.4-11.3-18-26.4-6.5-15.1-6.5-32.3v-0.5q0-17.2 6.5-32.5 6.6-15.2 18.3-26.6 11.7-11.3 27.5-17.9 15.8-6.5 34.5-6.5c12.3 0 23.8 2.2 34.4 6.5q15.8 6.6 27.3 17.8 11.5 11.2 18 26.4 6.6 15.1 6.6 32.3v0.5q0 17.2-6.6 32.5-6.5 15.2-18.2 26.6-11.7 11.3-27.6 17.9-15.8 6.5-34.4 6.5m0.3-40.1q9.2 0 16.8-3.5 7.6-3.4 12.9-9.3 5.3-5.8 8.3-13.6 3-7.8 3-16.5v-0.5c0-5.8-1-11.3-3-16.5-2-5.2-4.8-9.8-8.5-13.8q-5.5-5.9-13.1-9.5-7.6-3.6-16.8-3.6-9.2 0-16.7 3.5-7.5 3.4-12.9 9.3-5.4 5.8-8.3 13.6c-1.9 5.2-2.9 10.7-2.9 16.5v0.5c0 5.8 1 11.3 3 16.5 2 5.2 4.8 9.8 8.4 13.8q5.4 6 13 9.5 7.6 3.6 16.8 3.6"/>
+				</svg>
+			</div>
+			<div class="header__text">(ㅤScroll Downㅤ)</div>
+
+			<div class="slider">
+				<!-- Swiper untuk bagian kiri -->
+				<div class="swiper slider__left">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide">
+							<img src="./assets/images/250131 OSOI119996.jpg" alt="OSOI119996">
+						</div>
+						<div class="swiper-slide">
+							<img src="./assets/images/250131 OSOI119926.jpg" alt="OSOI119926">
+						</div>
+					</div>
+				</div>
+
+				<!-- Swiper untuk bagian kanan -->
+				<div class="swiper slider__right">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide">
+							<img src="./assets/images/250131 OSOI119926.jpg" alt="OSOI119926">
+						</div>
+						<div class="swiper-slide">
+							<img src="./assets/images/250131 OSOI119996.jpg" alt="OSOI119996">
+						</div>
+					</div>
+				</div>
+			</div>
 		</header>
 		
 		<div class="menubar">
@@ -69,7 +109,16 @@
 			
 		</footer>
 
+		<script src="./assets/vendors/jquery/jquery-3.7.1.min.js"></script>
+		<script src="./assets/vendors/swiper/swiper-bundle.min.js"></script>
 		<script defer src="./assets/js/script.js?<?= time() ?>"></script>
+
+		<img src="./assets/images/template/PC_A.png" alt="" style="
+			position: absolute;
+			top: 0;
+			opacity: 0.3;
+			z-index: 99999;
+		">
 		
 	</body>
 </html>
