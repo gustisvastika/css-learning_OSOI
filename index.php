07f835d59a630f4af88e2784b06bafb3a19aaaaf
gustisvastika <124154525+gustisvastika@users.noreply.github.com>
2025-02-18 10:53:23 +0800

    fix: change sp nav padding

---
+++ index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="ja" prefix="og: http://ogp.me/ns# fb: http://www.facebook.com/2008/fbml">
+	<head>			
+		<title>OSOI</title>
+		
+		<meta charset="UTF-8">
+		<meta name="description" content="description">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="robots" content="index, follow">
+		<meta name="author" content="">
+		<meta name="format-detection" content="telephone=no">
+		<meta name="Copyright" content="">
+		
+		<meta property="og:type" content="website">
+		<!-- <meta property="og:type" content="article"> -->
+		
+		<meta property="og:title" content="title">
+		<meta property="og:description" content="description">
+		<meta property="og:url" content="url">
+		<meta property="og:image" content="./assets/images/ogp.png">
+		<meta property="og:image:width" content="1200">
+		<meta property="og:image:height" content="630">
+		<meta property="og:site_name" content="title">
+		
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:title" content="title">
+		<meta name="twitter:description" content="description">
+		<meta name="twitter:url" content="url">
+		<meta name="twitter:image" content="./assets/images/ogp.png">
+
+		<link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
+		<link rel="apple-touch-icon" href="./assets/images/apple-touch-icon.png" sizes="180x180">
+		<link rel="icon" type="image/png" href="./assets/images/android-chrome-192x192.png" sizes="192x192">
+		
+		<link href="" rel="canonical">
+		
+		<link rel="stylesheet" href="./assets/vendors/animate-css/animate.min.css" />
+		<link rel="stylesheet" href="./assets/vendors/swiper/swiper-bundle.min.css" />
+		<link rel="stylesheet" type="text/css" href="./assets/css/style.css?<?= time() ?>">
+	</head>
+	<body style="position:relative;">
+		<main class="main">
+            <section id="hero" class="hero">
+                <div class="hero__logo">
+                    <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 602 210" width="300" height="100">
+                        <title>logo</title>
+                        <style>
+                            .s0 { fill: #ffffff } 
+                        </style>
+                        <path id="O1" class="s0" d="m87.5 187.5q-18.6 0-34.4-6.5-15.9-6.6-27.4-17.8-11.4-11.3-18-26.4-6.5-15.1-6.5-32.3v-0.5q0-17.2 6.5-32.5 6.6-15.2 18.3-26.6 11.7-11.3 27.5-17.9 15.8-6.5 34.5-6.5c12.3 0 23.8 2.2 34.4 6.5q15.8 6.6 27.3 17.8 11.5 11.2 18 26.4 6.6 15.1 6.6 32.3v0.5q0 17.2-6.6 32.5-6.5 15.2-18.2 26.6-11.7 11.3-27.6 17.9-15.8 6.5-34.4 6.5m0.3-40.1q9.2 0 16.8-3.5 7.6-3.4 12.9-9.3 5.3-5.8 8.3-13.6 3-7.8 3-16.5v-0.5c0-5.8-1-11.3-3-16.5-2-5.2-4.8-9.8-8.5-13.8q-5.5-5.9-13.1-9.5-7.6-3.6-16.8-3.6-9.2 0-16.7 3.5-7.5 3.4-12.9 9.3-5.4 5.8-8.3 13.6c-1.9 5.2-2.9 10.7-2.9 16.5v0.5c0 5.8 1 11.3 3 16.5 2 5.2 4.8 9.8 8.4 13.8q5.4 6 13 9.5 7.6 3.6 16.8 3.6"/>
+                        <path id="S" class="s0" d="m266.3 187.1q-21.5 0-41-6.7-19.3-6.7-34.3-20.1l24.8-29.7q24.1 19.3 52.1 19.3 9 0 13.7-2.9c3.1-1.9 4.7-4.6 4.7-8.1v-0.5q0-2.5-1.1-4.4-1.2-2-4.3-3.8-3.1-1.8-8.2-3.5-5.2-1.8-13.5-3.6-12.8-3-23.8-6.5-11.1-3.6-19.2-9.2-8.2-5.6-12.9-14-4.7-8.3-4.7-20.7v-0.4c0-7.5 1.4-14.4 4.2-20.7q4.3-9.4 12.4-16.2 8.2-6.9 19.7-10.7 11.5-3.8 25.9-3.8 20.7 0 36.9 5.5 16.2 5.4 29.3 16l-22.3 31.5q-11-7.8-22.6-11.8-11.6-4-22.4-4-8.1 0-12.1 3-4 3-4 7.3v0.5q0 2.7 1.3 4.8 1.2 2 4.3 3.8 3.1 1.7 8.5 3.4 5.4 1.7 13.7 3.5 13.8 3 24.9 7c7.4 2.7 13.7 5.9 18.9 9.8 5.3 3.8 9.2 8.4 12 13.8q4.1 8.2 4.1 19.4v0.4q0 12.4-4.7 22-4.7 9.7-13.2 16.4-8.5 6.8-20.4 10.3-12 3.6-26.7 3.6"/>
+                        <path id="O2" class="s0" d="m439.5 187.5q-18.6 0-34.5-6.5-15.8-6.6-27.3-17.8-11.5-11.3-18-26.4-6.6-15.1-6.6-32.3v-0.5q0-17.2 6.6-32.5 6.5-15.2 18.2-26.6 11.7-11.3 27.6-17.9 15.8-6.5 34.4-6.5 18.6 0 34.4 6.5 15.9 6.6 27.4 17.8 11.4 11.2 18 26.4 6.5 15.1 6.5 32.3v0.5q0 17.2-6.5 32.5-6.6 15.2-18.3 26.6-11.7 11.3-27.5 17.9-15.9 6.5-34.4 6.5m0.3-40.1q9.2 0 16.8-3.5 7.6-3.4 12.9-9.3 5.3-5.8 8.3-13.6 3-7.8 3-16.5v-0.5c0-5.8-1-11.3-3-16.5-2-5.2-4.9-9.8-8.5-13.8q-5.6-5.9-13.2-9.5-7.6-3.6-16.8-3.6-9.2 0-16.7 3.5-7.4 3.4-12.9 9.3-5.4 5.8-8.3 13.6-2.8 7.8-2.8 16.5v0.5c0 5.8 1 11.3 3 16.5 2 5.2 4.8 9.8 8.4 13.8q5.4 6 13 9.5c5 2.4 10.7 3.6 16.8 3.6"/>
+                        <path id="I" class="s0" d="m556.1 23.7h44.7v160.6h-44.7z"/>
+                    </svg>
+                </div>
+                <div class="hero__text">(ㅤScroll Downㅤ)</div>
+
+                <div class="slider">
+                    <div class="swiper slider__left">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119996.jpg" alt="OSOI119996"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119225.jpg" alt="OSOI119225"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119262.jpg" alt="OSOI119262"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119593.jpg" alt="OSOI119593"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119653.jpg" alt="OSOI119653"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119801.jpg" alt="OSOI119801"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119967.jpg" alt="OSOI119967"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI120133.jpg" alt="OSOI120133"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI120169.jpg" alt="OSOI120169"></div>
+                        </div>
+                    </div>
+
+                    <div class="swiper slider__right">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119926.jpg" alt="OSOI119926"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119122.jpg" alt="OSOI119122"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119129.jpg" alt="OSOI119129"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119481.jpg" alt="OSOI119481"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119726.jpg" alt="OSOI119726"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI119753.jpg" alt="OSOI119753"></div>
+                            <div class="swiper-slide"><img src="./assets/images/250131 OSOI120063.jpg" alt="OSOI120063"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+			<section id="section-description">
+				<div class="description">
+					<div class="description__title">
+						<img src="./assets/images/logo/logo_b.svg" alt="logo-OSOI" >
+						<h2>2025 SPRING</h2>
+					</div>
+					<div class="toggler toggler--language">
+						<input type="radio" id="en" name="language" value="EN" checked>
+						<label for="en">EN</label>
+
+						<input type="radio" id="jp" name="language" value="JP">
+						<label for="jp">JP</label>
+					</div>
+					<div class="description__content">
+						<p>
+							OSOI is a women's bag brand from Seoul, Korea,<br>
+							founded in 2016 by owner and creative director Hee Jin Kang.<br>
+							The brand name “OSOI” is derived from the Japanese word<br>
+							for “slow,” and it expresses the brand's desire to<br>
+							keep their own pace and create high-quality products,<br>
+							even if a little slowly.
+						</p>
+					</div>
+					<div class="description__footer">
+						<p>(ㅤWhich style to look at it from?ㅤ)</p>
+						<div class="category">
+                            <a class="category__item" data-category="gentle-lady" href="javascript:void(0)">Gentle Lady</a>
+                            <a class="category__item" data-category="culture-girl" href="javascript:void(0)">Culture Girl</a>
+                        </div>
+					</div>
+				</div>
+			</section>
+
+            <section id="section-product">
+                <nav class="nav">
+                    <div class="nav__logo">
+                        <img src="./assets/images/logo/logo_b.svg" alt="logo-OSOI" >
+                    </div>
+                    <div class="nav__item">
+                        <span id="nav-item-1">(ㅤGentle Ladyㅤ)</span>
+                        <span id="nav-item-2">Culture Girl</span>
+                        <a href="#">Online Store</a>
+                    </div>
+                </nav>
+                
+                <div id="gentle-lady">
+                    <div class="product-container">
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI120169.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price">Bag ¥44,000</div>
+                        </div>
+                        <div class="product product--category">
+                            <h1>Gentle Women</h1>
+                            <p>
+                                Stylish and always cool.<br> 
+                                Proud of who you are,<br>
+                                supple and dashing through life.
+                            </p>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119996.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119967.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image product__image--with-frame">
+                                <img src="./assets/images/250131 OSOI119653.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119593.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119262.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image product__image--right">
+                                <img src="./assets/images/250131 OSOI119225.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--multi">
+                                <span>Bag(hand) ¥44,000</span>
+                                <span>Bag(waist) ¥40,700</span>
+                            </div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image product__image--with-frame">
+                                <img src="./assets/images/250131 OSOI120133.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price">Bag [Exclusive Color ] ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119801.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                    </div>
+                </div>
+    
+                <div id="culture-girl">
+                    <div class="product-container">
+                        <div class="product product--category">
+                            <h1>Culture Girl</h1>
+                            <p>
+                                He loves subcultures, <br>
+                                and has a style that is not too kitschy, <br>
+                                but has a relaxed feel.
+                            </p>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119481.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119753.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image product__image--with-frame">
+                                <img src="./assets/images/250131 OSOI119726.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119926.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI120063.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥29,700 [Delivery : April]</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image product__image--with-frame">
+                                <img src="./assets/images/250131 OSOI119122.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price">Bag ¥44,000</div>
+                        </div>
+                        <div class="product">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119129.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white">Bag ¥44,000</div>
+                        </div>
+                        <div class="product product--big">
+                            <div class="product__image">
+                                <img src="./assets/images/250131 OSOI119409.jpg" alt="OSOI119926">
+                            </div>
+                            <div class="product__price product__price--white product__price--multi">
+                                <span>Bag(hand) ¥44,000</span>
+                                <span>Bag(waist) ¥40,700</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+		</main>
+		
+		<footer class="footer">
+            <div class="store-link">
+                <a href="javascript:void(0)">Online Store</a>
+                <a href="javascript:void(0)">Instagram</a>
+            </div>
+
+            <div class="media-link">
+                <p>Share on</p>
+                <div class="media-link__icon">
+                    <a href="javascript:void(0)"><img src="./assets/images/logo/line.svg" alt="logo-OSOI"></a>
+                    <a href="javascript:void(0)"><img src="./assets/images/logo/x-twitter.svg" alt="logo-OSOI"></a>
+                    <a href="javascript:void(0)"><img src="./assets/images/logo/facebook.svg" alt="logo-OSOI"></a>
+                </div>
+            </div>
+
+            <div class="footer__copyright">
+                © UNITED ARROWS LTD. ALL RIGHTS RESERVED.
+            </div>
+		</footer>
+
+		<script src="./assets/vendors/jquery/jquery-3.7.1.min.js"></script>
+		<script src="./assets/vendors/swiper/swiper-bundle.min.js"></script>
+		<script defer src="./assets/js/script.js?<?= time() ?>"></script>
+
+		<!-- <img src="./assets/images/template/PC_A.png" alt="" style="
+			position: absolute;
+			top: 0;
+			opacity: 0.3;
+			z-index: 99999;
+		"> -->
+	</body>
+</html>
